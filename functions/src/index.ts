5b6cd125a9963ef0df6246b0e8f553c4e86d7844
yudai2929 <yudai201729@icloud.com>
2023-05-14 17:16:34 +0900

    👍feat: middleware for error handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,15 @@ import * as functions from "firebase-functions"
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true })
-  response.send("Hello from Firebase!")
-})
+export const helloWorld = functions.https.onRequest((request, response) =>
+  middleware(request, response, () => {
+    console.log("helloWorld")
+    response.send("Hello from Firebase!")
+  })
+)
+
+const middleware = (req: functions.Request, res: functions.Response, next: () => void) => {
+  console.log("middleware start")
+  next()
+  console.log("middleware end")
+}
