7880bc4b262d9d225baad3be2762b48f27c9bd96
yudai2929 <yudai201729@icloud.com>
2023-05-28 16:55:38 +0900

    delete: unnecessary code

--- functions/src/firebase/config.ts
+++ functions/src/firebase/config.ts
@@ -1,6 +1,5 @@
 export * as functions from "firebase-functions"
 import * as firebase from "firebase-admin"
-import * as functions from "firebase-functions"
 
 firebase.initializeApp()
 export const db = firebase.firestore()
